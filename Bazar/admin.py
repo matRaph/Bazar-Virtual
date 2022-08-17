ea1a86c2d227f6ff481da5ba8578ee93f1e9ac7f
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-17 14:59:42 -0300

    Definindo e migrando modelos do branco de dados

--- Bazar/admin.py
+++ Bazar/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Evento, Item, Usuario
+
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Item)
+admin.site.register(Evento)

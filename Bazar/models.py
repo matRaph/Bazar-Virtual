00a32409890968e2008d28557b45b511551c26df
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-27 01:12:26 -0300

    Adicionando campo evento ao model item

--- Bazar/models.py
+++ Bazar/models.py
@@ -6,24 +6,6 @@ from django.db import models
 
 # Create your models here.
 
-
-class Item(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    imagem = models.ImageField(upload_to='imagens', null=True, blank=True)
-    dono = models.ForeignKey(User, on_delete=models.CASCADE, 
-        related_name='dono')
-    usuario_reserva = models.ForeignKey(User, on_delete=models.CASCADE, 
-        null=True, blank=True, related_name='usuario_reserva')
-    
-    class Meta:
-        verbose_name = 'Item'
-        verbose_name_plural = 'Itens'
-        
-    def __str__(self):
-        return self.nome
-
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
     data_inicial = models.DateField()
@@ -35,3 +17,21 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.nome
+        
+class Item(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='imagens', null=True, blank=True)
+    dono = models.ForeignKey(User, on_delete=models.CASCADE, 
+        related_name='dono')
+    usuario_reserva = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True, related_name='usuario_reserva')
+    evento = models.ManyToManyField(Evento, related_name='evento')
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Itens'
+        
+    def __str__(self):
+        return self.nome
+
+
